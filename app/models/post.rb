6f7798a15e4bd506f464b24c3d2ef6fe937c6f5c
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-22 17:55:54 -0500

    commit tree 84bcfe45cf55e25c814ba862ff67fd1b25914050

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait 
+
+    def clickbait
+    
+        if !title
+            errors.add(:title, "No")
+        else
+
+        unless   title.include? ( "You Won't Believe"||  "Secret" ||"Guess" ||  "Top ")
+                errors.add(:title, "No")
+         end
+        end
+            
+        end
+
 end
